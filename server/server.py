44d58ad28b0232d4f7031283f04f255622c6a7da
Thibault BRONCHAIN <thibault@mc2.io>
2013-12-07 18:43:47 +0800

    base

---
+++ server/server.py
@@ -0,0 +1,81 @@
+import tornado.web, tornado.ioloop
+import motor
+
+HTML_LOCATION="./html"
+
+DEMO_USER={
+    "phone": "",
+    "address": "",
+    "status":0
+    }
+
+DEMO_AYI=[
+    {
+        "name": "",
+        "phone": "",
+        "area": ""
+        },
+    {
+        "name": "",
+        "phone": "",
+        "area": ""
+        }
+    ]
+
+class FindHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def post(self):
+        """Finding an Ayi
+        """
+        position = self.get_argument('position')
+        
+
+        response = {
+            'status': "OK"
+            }
+        DEMO_USER["status"] = 1
+        self.write(response)
+
+#    def _on_response(self, result, error):
+#        if error:
+#            raise tornado.web.HTTPError(500, error)
+#        else:
+#            self.redirect('/')
+
+class StatusHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def post(self):
+        """Getting status
+        """
+        id = self.get_argument('id')
+
+        response = {
+            'status': DEMO_USER.get("status",0)
+            }
+        self.write(response)
+
+class PageHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        """Display Homepage
+        """
+        with open("%s/index.html"%(HTML_LOCATION), 'r') as content_file:
+            content = content_file.read()
+        self.write(content)
+        self.finish()
+
+#db = motor.MotorClient().open_sync().maideasy
+
+application = tornado.web.Application(
+    [
+        (r'/api/find', FindHandler),
+        (r'/api/status', StatusHandler),
+        (r'/', PageHandler)
+    ],
+#    db=db
+)
+
+if __name__ == "__main__":
+    print 'Starting ...'
+    application.listen(80)
+    tornado.ioloop.IOLoop.instance().start()
